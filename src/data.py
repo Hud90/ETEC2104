73badea97fa2a84f50d53de28f208704d88ac761
Hud90 <hmatthews04@gmail.com>
2025-01-30 15:04:05 -0500

    Finished mock draft of templates.

--- src/data.py
+++ src/data.py
@@ -2,6 +2,6 @@ names = ["Blake","Bob","Billy","Crystal","Derick","Donnie","Edgard","Frank","Gar
 
 image_list = ["/html/logo1.png","/html/logo2.png","/html/logo3.png","/html/logo4.png","/html/logo5.png","/html/logo6.png","/html/logo7.png","/html/logo8.png","/html/logo9.png","/html/logo10.png"]
 
-post_age = []
+post_age = ["1 hour ago", "3 hours ago","1 week ago","2 weeks ago","1 month ago","3 months ago","1 year ago","2 years ago","8 years ago","1 week ago"]
 
-views = []
+views = ["240 Views","1.3K views","4K views","123 views","52K views","3.2M views","4M views","303K views","22K views","5.5K views"]
--- src/page_posts.py
+++ src/page_posts.py
@@ -8,6 +8,8 @@ srcdir = os.path.dirname(__file__)
 
 def get():
     shuffled_images = random.sample(data.image_list, len(data.image_list))  # Shuffle images
+    shuffled_age = random.sample(data.post_age, len(data.post_age))
+    shuffled_views = random.sample(data.views, len(data.views))
     T = mako.template.Template(filename=f"{srcdir}/posts.html")
-    return T.render(image_list=shuffled_images)
+    return T.render(image_list=shuffled_images, post_age=shuffled_age, views=shuffled_views)
     
